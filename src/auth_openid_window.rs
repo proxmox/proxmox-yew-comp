dce50771d7ebfef57846ab3baef9e046cf496e18
Dietmar Maurer <dietmar@proxmox.com>
2023-09-15 17:31:36 +0200

    AuthOpenIDWindow: add missing form fields

--- src/auth_openid_window.rs
+++ src/auth_openid_window.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use pwt::widget::form::FormContext;
+use pwt::widget::form::{FormContext, Boolean, Combobox};
 use yew::html::{IntoEventCallback, IntoPropValue};
 use yew::virtual_dom::{VComp, VNode};
 
@@ -43,10 +43,67 @@ pub struct ProxmoxAuthOpenIDWindow {}
 fn render_input_form(form_ctx: FormContext, props: AuthOpenIDWindow) -> Html {
     let is_edit = props.realm.is_some();
 
+    let username_claim_items = Rc::new(vec![
+        "subject".into(),
+        "username".into(),
+        "email".into(),
+    ]);
+
+    let prompt_items = Rc::new(vec![
+        "none".into(),
+        "login".into(),
+        "consent".into(),
+        "select_account".into(),
+    ]);
+
     InputPanel::new()
         .show_advanced(form_ctx.get_show_advanced())
         .class("pwt-p-2")
-        .with_field(tr!("Realm"), Field::new().name("realm").disabled(is_edit))
+        .with_large_field(
+            tr!("Issuer URL"),
+            Field::new().name("issuer-url").required(true)
+        )
+
+        .with_field(
+            tr!("Realm"),
+            Field::new().name("realm").required(true).disabled(is_edit))
+        .with_right_field(
+            tr!("Autocreate Users"),
+            Boolean::new().name("autocreate")
+        )
+
+        .with_field(
+            tr!("Client ID"),
+            Field::new().name("client-id").required(true))
+        .with_right_field(
+            tr!("Username Claim"),
+            Combobox::new()
+                .name("username-claim")
+                .disabled(is_edit)
+                .editable(true)
+                .placeholder(tr!("Default"))
+                .items(username_claim_items)
+        )
+
+        .with_field(
+            tr!("Client Key"),
+            Field::new().name("client-key"))
+        .with_right_field(
+            tr!("Scopes"),
+            Field::new()
+                .name("scopes")
+                .placeholder(tr!("Default") + " (" + &tr!("email profile") + ")")
+        )
+
+        .with_field(
+            tr!("Prompt"),
+            Combobox::new()
+                .name("prompt")
+                .editable(true)
+                .placeholder(tr!("Auth-Provider Default"))
+                .items(prompt_items)
+        )
+
         .with_large_field(tr!("Comment"), Field::new().name("comment"))
         .with_advanced_spacer()
         .with_advanced_field(tr!("ACR Values"), Field::new().name("acr-values"))
