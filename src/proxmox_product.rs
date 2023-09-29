2583c453d7c3630e8ab9baefff5c661bf9b35393
Dietmar Maurer <dietmar@proxmox.com>
2023-09-29 17:46:22 +0200

    ProxmoxProduct: add product text

--- src/proxmox_product.rs
+++ src/proxmox_product.rs
@@ -1,5 +1,7 @@
 //! Define product specific things here
 
+use pwt::tr;
+
 /// Enumerate the different Proxmox products.
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub enum ProxmoxProduct {
@@ -16,6 +18,15 @@ impl ProxmoxProduct {
             ProxmoxProduct::PMG => "PMGAuthCookie",
             ProxmoxProduct::PBS => "PBSAuthCookie",
             ProxmoxProduct::POM => "POMAuthCookie",
+        }
+    }
+
+    pub fn product_text(&self) -> String {
+        match self {
+            ProxmoxProduct::PVE => tr!("Proxmox Virtual Environment"),
+            ProxmoxProduct::PMG => tr!("Proxmox Mail Gateway"),
+            ProxmoxProduct::PBS => tr!("Proxmox Backup Server"),
+            ProxmoxProduct::POM => tr!("Proxmox Offline Mirror"),
         }
     }
 }
