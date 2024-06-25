83e77c140ec6ffe6042ea752a4a1a06ad704c9b7
Dominik Csapak <d.csapak@proxmox.com>
2024-06-25 15:14:40 +0200

    syslog: add 'live mode' with new journal view
    very similar to what we have implemented in extjs, have a segmented
    button to change between live mode and timespan selection, and change
    the latter to not automatically load when at the bottom (that's what the
    live mode is for, which does this more efficiently)
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/syslog.rs
+++ src/syslog.rs
@@ -1,14 +1,14 @@
 use std::rc::Rc;
 
 use pwt::widget::form::InputType;
-use pwt::widget::Container;
+use pwt::widget::{Button, Container, Row, SegmentedButton};
 use yew::html::IntoPropValue;
 use yew::virtual_dom::{VComp, VNode};
 
 use pwt::prelude::*;
 use pwt::widget::{form::Field, Column, Toolbar};
 
-use crate::LogView;
+use crate::{JournalView, LogView};
 
 use pwt_macros::builder;
 
@@ -18,8 +18,12 @@ pub struct Syslog {
     /// Base URL for the syslog API
     #[prop_or("/nodes/localhost/syslog".into())]
     #[builder(IntoPropValue, into_prop_value)]
-    /// The base url for
     pub base_url: AttrValue,
+
+    /// Base URL for the journal API
+    #[prop_or("/nodes/localhost/journal".into())]
+    #[builder(IntoPropValue, into_prop_value)]
+    pub journal_base_url: AttrValue,
 
     /// Show logs for specified service.
     #[builder(IntoPropValue, into_prop_value)]
@@ -34,6 +38,7 @@ impl Syslog {
 }
 
 pub enum Msg {
+    ChangeMode(bool),
     LoadingChange((usize, bool)),
     Since(String),
     Until(String),
@@ -84,10 +89,24 @@ impl ProxmoxSyslog {
             )
             .with_flex_spacer()
             .with_child(
+                SegmentedButton::new()
+                    .with_button(
+                        Button::new(tr!("Live Mode"))
+                            .pressed(self.active)
+                            .onclick(ctx.link().callback(|_| Msg::ChangeMode(true))),
+                    )
+                    .with_button(
+                        Button::new(tr!("Select Timespan"))
+                            .pressed(!self.active)
+                            .onclick(ctx.link().callback(|_| Msg::ChangeMode(false))),
+                    ),
+            )
+            .with_child(
                 Container::from_tag("label")
                     .id(self.since_label_id.clone())
                     .padding_start(2)
                     .class("pwt-align-self-center")
+                    .class(self.active.then_some("pwt-label-disabled"))
                     .with_child("Since:"),
             )
             .with_child(
@@ -95,6 +114,7 @@ impl ProxmoxSyslog {
                     .label_id(self.since_label_id.clone())
                     .input_type(InputType::DatetimeLocal)
                     .required(true) // avoid clear button in firefox
+                    .disabled(self.active)
                     .class("pwt-input-hide-clear-button")
                     .on_change(ctx.link().callback(Msg::Since))
                     .value(since),
@@ -104,6 +124,7 @@ impl ProxmoxSyslog {
                     .id(self.since_label_id.clone())
                     .padding_start(2)
                     .class("pwt-align-self-center")
+                    .class(self.active.then_some("pwt-label-disabled"))
                     .with_child("Until:"),
             )
             .with_child(
@@ -111,6 +132,7 @@ impl ProxmoxSyslog {
                     .label_id(self.until_label_id.clone())
                     .input_type(InputType::DatetimeLocal)
                     .required(true) // avoid clear button in firefox
+                    .disabled(self.active)
                     .on_change(ctx.link().callback(Msg::Until))
                     .value(until),
             )
@@ -120,30 +142,37 @@ impl ProxmoxSyslog {
 
     fn create_log_view(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
-
-        let since = if self.since.get_date() == 0 {
-            // invalid data (clear field creates this)
-            get_default_since()
+        if self.active {
+            JournalView::new(props.journal_base_url.clone())
+                .on_loading_change(ctx.link().callback(|(loading, tailview)| {
+                    Msg::LoadingChange((if loading { 1 } else { 0 }, tailview))
+                }))
+                .into()
         } else {
-            self.since.clone()
-        };
+            let since = if self.since.get_date() == 0 {
+                // invalid data (clear field creates this)
+                get_default_since()
+            } else {
+                self.since.clone()
+            };
 
-        let until = if self.until.get_date() == 0 {
-            // invalid data (clear field creates this)
-            None
-        } else {
-            Some((self.until.get_time() / 1000.0) as i64)
-        };
+            let until = if self.until.get_date() == 0 {
+                // invalid data (clear field creates this)
+                None
+            } else {
+                Some((self.until.get_time() / 1000.0) as i64)
+            };
 
-        LogView::new(props.base_url.clone())
-            .padding(2)
-            .class("pwt-flex-fill")
-            .service(props.service.clone())
-            .since((since.get_time() / 1000.0) as i64)
-            .until(until)
-            .active(self.active)
-            .on_pending_change(ctx.link().callback(Msg::LoadingChange))
-            .into()
+            LogView::new(props.base_url.clone())
+                .padding(2)
+                .class("pwt-flex-fill")
+                .service(props.service.clone())
+                .since((since.get_time() / 1000.0) as i64)
+                .until(until)
+                .active(false)
+                .on_pending_change(ctx.link().callback(Msg::LoadingChange))
+                .into()
+        }
     }
 }
 
@@ -201,6 +230,11 @@ impl Component for ProxmoxSyslog {
                 let new_pending = num > 0 && !tail_view;
                 let changed = new_pending != self.pending;
                 self.pending = new_pending;
+                changed
+            }
+            Msg::ChangeMode(active) => {
+                let changed = active != self.active;
+                self.active = active;
                 changed
             }
         }
