f7a7f880a6d2f20043a19cfbcd697cde4b4fbb19
Shannon Sterz <s.sterz@proxmox.com>
2024-12-20 15:16:26 +0100

    acme: use `as_str` instead of re- and dereferencing
    this check trips up clippy fixes, as `&*` should be redundant if both
    sides try to take reference. however, due to how this type is
    encapsulated, that won't work and break the check. so use `as_str` to
    get a reference to a string slice for with comparison with `String` is
    properly implemented.

--- src/acme/acme_dirtectory_selector.rs
+++ src/acme/acme_dirtectory_selector.rs
@@ -121,7 +121,7 @@ impl Component for ProxmoxAcmeDirectorySelector {
             .render_value({
                 let store = self.store.clone();
                 move |url: &AttrValue| {
-                    let text = match store.read().data().iter().find(|item| &item.url == &*url) {
+                    let text = match store.read().data().iter().find(|item| &item.url == url.as_str()) {
                         Some(entry) => entry.name.clone(),
                         None => url.to_string(),
                     };
