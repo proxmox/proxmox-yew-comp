proxmox/proxmox-yew-comp

commit d5f3c95eabf85e1d4889139020339d50ce4d7787

wizard: allow enter to be used for switching to next page
by intercepting the `onsubmit` event of the form for each page, which is
triggered when pressing enter in an input field.

For it to work on all forms, we have to inject an invisible input
'submit' element, since pressing enter only submits the form if such an
element is present (or if the form contains a single field; which we
cannot check here).

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/wizard.rs
@@ -17,8 +17,8 @@ use pwt::props::{AsCssStylesMut, ContainerBuilder, CssStyles};
 use pwt::state::Selection;
 use pwt::widget::form::{Form, FormContext};
 use pwt::widget::{
-    AlertDialog, Button, Container, Dialog, Mask, MiniScrollMode, Row, TabBarItem, TabBarStyle,
-    TabPanel,
+    AlertDialog, Button, Container, Dialog, Input, Mask, MiniScrollMode, Row, TabBarItem,
+    TabBarStyle, TabPanel,
 };
 
 use yew::html::{IntoEventCallback, IntoPropValue};
@@ -467,7 +467,7 @@ impl Component for PwtWizard {
         let state = self.controller.read();
 
         let mut disabled = false;
-        for (key, page) in props.pages.iter() {
+        for (page_num, (key, page)) in props.pages.iter().enumerate() {
             let active = Some(key) == state.page.as_ref();
             let form_ctx = state.page_data.get(key).unwrap();
 
@@ -479,11 +479,30 @@ impl Component for PwtWizard {
                 controller: self.controller.clone(),
             });
 
+            let next_page = props
+                .pages
+                .get_index(page_num + 1)
+                .map(|(key, _)| key.clone());
+
             let page_content = Form::new()
                 .class(Overflow::Auto)
                 .class(Flex::Fill)
                 .form_context(form_ctx.clone())
-                .with_child(page_content);
+                .onsubmit(ctx.link().batch_callback({
+                    let form_ctx = form_ctx.clone();
+                    move |_| {
+                        if !form_ctx.read().is_valid() {
+                            return None;
+                        }
+                        if let Some(page) = next_page.clone() {
+                            Some(Msg::SelectPage(page, true))
+                        } else {
+                            Some(Msg::Submit)
+                        }
+                    }
+                }))
+                .with_child(page_content)
+                .with_child(Input::new().attribute("type", "submit").class("pwt-d-none"));
 
             let tab_bar_item = page.tab_bar_item.clone().disabled(disabled);
 

commit cb65b4f03dccecd834e0932887b6648b7394b5e9

wizard: fix form progressing when pressing enter
commit
d5f3c95 (wizard: allow enter to be used for switching to next page)

intended the current page to be progressed with enter the same way
the next button is pressed.

The check for this was wrong though, since not only the form context
validity is relevant, also the page_lock state and previous page
validity state.

To fix this, use the WizardState's `can_progress` check we now have
instead, which is also used to enable/disable the `next` button.

Fixes: d5f3c95 (wizard: allow enter to be used for switching to next page)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/wizard.rs
@@ -523,9 +523,9 @@ impl Component for PwtWizard {
                 .class(Flex::Fill)
                 .form_context(form_ctx.clone())
                 .onsubmit(ctx.link().batch_callback({
-                    let form_ctx = form_ctx.clone();
+                    let state = self.controller.clone();
                     move |_| {
-                        if !form_ctx.read().is_valid() {
+                        if !state.read().can_progress() {
                             return None;
                         }
                         if let Some(page) = next_page.clone() {