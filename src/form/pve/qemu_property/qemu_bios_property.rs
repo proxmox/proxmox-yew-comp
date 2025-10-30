a31f97273dea7277355cb6d0fadd1a6210e53c1a
Dietmar Maurer <dietmar@proxmox.com>
2025-10-30 14:37:20 +0100

    qemu bios property: use InputPanel for mobile view
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- src/form/pve/qemu_property/qemu_bios_property.rs
+++ src/form/pve/qemu_property/qemu_bios_property.rs
@@ -1,13 +1,13 @@
 use pwt::prelude::*;
 use pwt::widget::form::Combobox;
-use pwt::widget::{Column, Container};
+use pwt::widget::{Container, FieldPosition, InputPanel};
 
 use pve_api_types::QemuConfigBios;
 
 use crate::form::delete_empty_values;
 use crate::{EditableProperty, PropertyEditorState, RenderPropertyInputPanelFn};
 
-fn input_panel(_mobile: bool) -> RenderPropertyInputPanelFn {
+fn input_panel(mobile: bool) -> RenderPropertyInputPanelFn {
     RenderPropertyInputPanelFn::new(move |state: PropertyEditorState| {
         let form_ctx = state.form_ctx;
         let show_efi_disk_hint =
@@ -15,24 +15,36 @@ fn input_panel(_mobile: bool) -> RenderPropertyInputPanelFn {
 
         let hint = |msg: String| Container::new().class("pwt-color-warning").with_child(msg);
 
-        //let bios_label = "BIOS";
+        let efidisk_hint = hint(tr!(
+            "You need to add an EFI disk for storing the EFI settings. See the online help for details."
+        ));
+
+        let bios_label = "BIOS";
         let bios_field =
             Combobox::from_key_value_pairs([("ovmf", "OVMF (UEFI)"), ("seabios", "SeaBIOS")])
                 .name("bios")
                 .submit_empty(true)
                 .placeholder("SeaBIOS");
 
-        Column::new()
+        let mut panel = InputPanel::new()
+            .mobile(mobile)
             .class(pwt::css::FlexFit)
             .padding_x(2)
-            .gap(2)
-            .padding_bottom(1) // avoid scrollbar ?!
-            .with_child(bios_field)
-            .with_optional_child(show_efi_disk_hint.then(|| {
-                hint(tr!(
-                    "You need to add an EFI disk for storing the EFI settings. See the online help for details."
-                ))
-            }))
+            .padding_bottom(1); // avoid scrollbar ?!
+
+        if mobile {
+            panel.add_custom_child(bios_field);
+        } else {
+            panel.add_field(bios_label, bios_field);
+        }
+
+        panel
+            .with_custom_child_and_options(
+                FieldPosition::Left,
+                false,
+                !show_efi_disk_hint,
+                efidisk_hint,
+            )
             .into()
     })
 }
