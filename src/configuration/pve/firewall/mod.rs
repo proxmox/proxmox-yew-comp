61eff8346042d95bfd87656e483707526f5490ac
Dietmar Maurer <dietmar@proxmox.com>
2026-01-14 10:26:59 +0100

    cleanup: move all pve firewall configuration to separate directory
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

---
+++ src/configuration/pve/firewall/mod.rs
@@ -0,0 +1,8 @@
+mod firewall_options_cluster_panel;
+pub use firewall_options_cluster_panel::FirewallOptionsClusterPanel;
+
+mod firewall_options_guest_panel;
+pub use firewall_options_guest_panel::FirewallOptionsGuestPanel;
+
+mod firewall_options_node_panel;
+pub use firewall_options_node_panel::FirewallOptionsNodePanel;
